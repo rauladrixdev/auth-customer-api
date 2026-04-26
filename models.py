83d88bc18d51c765db7c3ed0676257b27c98fa44
Raul <raum.adrix@icloud.com>
2026-04-26 20:28:07 -0300

    auth-customer-api

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String
 from database import Base
 
 class User(Base):
-    _tablename_ = "users"
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
     email = Column(String, unique=True)
